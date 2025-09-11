f8d07aee16d12667bf7b8da9ecbbd0f3af8c92b7
ramakant <gachiramakant75@gmail.com>
2025-09-11 12:50:35 +0530

    updated audit log with try catch

--- src/main/java/com/example/societyfest/service/AuditLogService.java
+++ src/main/java/com/example/societyfest/service/AuditLogService.java
@@ -5,12 +5,14 @@ import com.example.societyfest.repository.AuditLogRepository;
 import com.example.societyfest.util.JsonUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuditLogService {
@@ -27,7 +29,7 @@ public class AuditLogService {
         String ip = request.getRemoteAddr();
         String userAgent = request.getHeader("User-Agent");
 
-        AuditLog log = AuditLog.builder()
+        AuditLog auditlog = AuditLog.builder()
                 .username(username)
                 .action(action)
                 .entityType(entityType)
@@ -38,8 +40,11 @@ public class AuditLogService {
                 .userAgent(userAgent)
                 .timestamp(LocalDateTime.now())
                 .build();
-
-        auditLogRepository.save(log);
+        try {
+            auditLogRepository.save(auditlog);
+        }catch (Exception e){
+            log.error("Failed to save audit log: {}", e.getMessage());
+        }
     }
 
     private String getCurrentUsername() {
